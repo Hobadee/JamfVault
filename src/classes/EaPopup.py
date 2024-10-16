cfb55466b182155cce5e79ba3f6ac6fedab2759f
Eric Kincl <eric@kincl.net>
2024-10-15 23:36:56 -0700

    Flushed out the EaPopup class some
    Copied in basic functions to make iterable, indexable, remove items, check length

--- src/classes/EaPopup.py
+++ src/classes/EaPopup.py
@@ -1,14 +1,64 @@
 from __future__ import annotations
 
 class EaPopup:
-    """
-    I'm not entirely sure I want this as it's own class right now.
-
-    ...I do, I just don't want to deal with implementation right now.
-    """
     
     def __init__(self):
         self._choices = []
 
+        # Index for iterator
+        self._index = 0
+
+
     def addChoice(self, choice):
         self._choices.append(choice)
+
+
+    def add(self, popup):
+        self._choices.append(popup)
+
+
+    def remove(self, popup):
+        self._choices.remove(popup)
+
+
+    def len(self):
+        return self.__len__()
+
+
+    def __len__(self):
+        """
+        Add support for the len() command
+        """
+        return len(self._choices)
+
+
+    # Make indexable
+    def __getitem__(self, index):
+        return self._choices[index]
+
+    def __setitem__(self, index, newvalue):
+        self._choices[index] = newvalue
+
+
+
+    def __contains__(self, item):
+        for current_item in self._choices:
+            if item == current_item:
+                return True
+        return False
+
+
+
+    # Make iterable
+    def __iter__(self):
+        return iter(self._choices)
+
+    
+    def __next__(self):
+        if self._index < len(self._choices):
+            s = self._choices[self._index]
+            self._index += 1
+            return s
+        else:
+            raise StopIteration
+    
